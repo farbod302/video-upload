0275e9d5bd92d92b1d72075135e6d80a1a0318c9
farbod302 <f.aliakbari302@gmail.com>
2024-10-07 17:40:43 +0330

    init

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+app.use(bodyParser.urlencoded({ limit: "1024mb", extended: true }))
+app.use(bodyParser.json({ extended: true }))
+const http = require("http")
+const server = http.createServer(app)
+server.listen("4010")
+const fs = require("fs")
+const { uid } = require("uid")
+const { Worker } = require('worker_threads');
+const multer = require("multer")
+app.post("/create_folder", (req, res) => {
+    const { name } = req.body
+    try {
+        if (!fs.existsSync(`${__dirname}/videos/${name}`)) {
+            fs.mkdirSync(`${__dirname}/videos/${name}`);
+            res.json({ status: true })
+        } else {
+            res.json({ status: false, msg: "Already exist" })
+        }
+    } catch (err) {
+        res.json({ status: false })
+    }
+})
+
+app.get("/get_folders", (req, res) => {
+    const folders = fs.readdirSync(`${__dirname}/videos`)
+    res.json({ status: true, folders })
+})
+
+
+const upload = multer({ dest: `${__dirname}/temp` })
+
+app.post("/upload", upload.single("video"), (req, res) => {
+    if (!req.file) {
+        res.json({
+            status: false, msg: "Invalid params"
+        })
+    }
+
+    const input = req.file
+    const { folder, name } = req.body
+    const id = uid(8)
+    const output_path = `${__dirname}/videos/${folder || "default"}/${id}.mp4`
+    const { path } = input
+    const to_add = {
+        id,
+        input_path: path,
+        output_path,
+        status: "queue",
+        percent: 0,
+        name
+
+    }
+    const json_str = fs.readFileSync(`${__dirname}/videos.json`)
+    const json = JSON.parse(json_str.toString())
+    json.push(to_add)
+    fs.writeFileSync(`${__dirname}/videos.json`, JSON.stringify(json))
+    run_queue()
+    res.json({ status: true, msg: "Added to queue" })
+
+})
+
+const run_queue = () => {
+    const json_str = fs.readFileSync(`${__dirname}/videos.json`)
+    const json = JSON.parse(json_str.toString())
+    const is_progress = json.some(e => e.status === "in_progress")
+    if (is_progress) return
+    const progress = json.find(e => e.status === "queue")
+    if (!progress) return
+    const index = json.findIndex(e => e.status === "queue")
+    json[index].status = "in_progress"
+    fs.writeFileSync(`${__dirname}/videos.json`, JSON.stringify(json))
+    const { input_path, output_path, id } = progress
+    const worker = new Worker("./worker.js", { workerData: { inputFilePath: input_path, outputFilePath: output_path, id } })
+    worker.on("message", async (msg) => {
+        const { status } = msg
+        if (status === "error") {
+            const json_str = fs.readFileSync(`${__dirname}/videos.json`)
+            const json = JSON.parse(json_str.toString())
+            const index = json.findIndex(e => e.id === id)
+            json[index].status = "failed"
+            fs.writeFileSync(`${__dirname}/videos.json`, JSON.stringify(json))
+            run_queue()
+            return
+        }
+        const json_str = fs.readFileSync(`${__dirname}/videos.json`)
+        const json = JSON.parse(json_str.toString())
+        const index = json.findIndex(e => e.id === id)
+        json[index].status = "complete"
+        fs.writeFileSync(`${__dirname}/videos.json`, JSON.stringify(json))
+        run_queue()
+        return status === "done"
+    })
+
+}
---
+++ worker.js
@@ -0,0 +1,43 @@
+const { parentPort, workerData } = require('worker_threads');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+const ffprobeInstaller = require('@ffprobe-installer/ffprobe');
+const fs = require('fs');
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+ffmpeg.setFfprobePath(ffprobeInstaller.path);
+
+function convertAndCompress(inputFilePath, outputFilePath, id) {
+    return new Promise((resolve, reject) => {
+        ffmpeg(inputFilePath)
+            .videoCodec('libx264')
+            .format('mp4')
+            .outputOptions('-preset fast')
+            .outputOptions('-crf 28')
+            .on('end', () => {
+                resolve(outputFilePath);
+            })
+            .on('error', (err) => {
+                reject(err);
+            })
+            .on("progress", (e) => {
+                const json_str = fs.readFileSync(`${__dirname}/videos.json`)
+                const json = JSON.parse(json_str.toString())
+                const index = json.findIndex(e => e.id === id)
+                json[index].percent = Math.floor(e.percent)
+                fs.writeFileSync(`${__dirname}/videos.json`, JSON.stringify(json))
+            })
+            .save(outputFilePath);
+    });
+}
+
+(async () => {
+    try {
+        await convertAndCompress(workerData.inputFilePath, workerData.outputFilePath, workerData.id);
+        parentPort.postMessage({ status: 'done', outputFilePath: workerData.outputFilePath });
+    } catch (err) {
+        parentPort.postMessage({ status: 'error', message: err.message });
+    } finally {
+        fs.unlink(workerData.inputFilePath, (err) => { if (err) console.error(err); });
+    }
+})();
