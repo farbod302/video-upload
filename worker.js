8253123861e31894d38525fc07cc4f28979f07b2
farbod302 <f.aliakbari302@gmail.com>
2024-10-08 20:27:00 +0330

    change qulity

--- worker.js
+++ worker.js
@@ -20,7 +20,7 @@ function convertAndCompress(inputFilePath, outputFilePath, id) {
             .videoCodec('libx264')
             .format('mp4')
             .outputOptions('-preset fast')
-            .outputOptions('-crf 28')
+            .outputOptions('-crf 18')
             .on('end', () => {
                 resolve(outputFilePath);
             })
