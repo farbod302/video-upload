6c5b090de5d899b73a3fc0723b68e50281723092
farbod302 <f.aliakbari302@gmail.com>
2024-10-26 21:28:28 +0330

    remove file name

--- index.js
+++ index.js
@@ -181,7 +181,6 @@ app.post("/motivation", upload.single("video"), (req, res) => {
             const blob = new Blob([output])
             const form_data = new FormData()
             form_data.append("files", blob, `${id}.mp4`)
-            form_data.append("filename", `${id}.mp4`)
             await fetch(
                 `https://www.nutrosal.com/saveMotivationImage/Nutrosal/${user_id}`,
                 {
