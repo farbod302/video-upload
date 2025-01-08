7f1b94d4fe2292177e28cbb111b4e908c0f368f3
farbod302 <f.aliakbari302@gmail.com>
2025-01-08 16:08:01 +0330

    packages map

--- packagesMap.js
+++ packagesMap.js
@@ -139,6 +139,233 @@ const _packages_sessions = {
                 ]
             }
         ]
+    },
+    "3": {
+        name: "Workout",
+        img: "/face-yoga/land-image.jpg",
+        videos: [
+            {
+                session: "Session 1",
+                episodes: [
+                    {
+                        name: "1-Intro",
+                        duration: "",
+                        url:""
+                    },
+                    {
+                        name: "2-Squat",
+                        duration: "2:13",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/becbb9f6.mp4"
+                    },
+                    {
+                        name: "3-Lunges",
+                        duration: "2:38",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/ecbb9f66.mp4"
+                    },
+                    {
+                        name: "4-Bridge",
+                        duration: "1:49",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/cbb9f665.mp4"
+                    },
+                    {
+                        name: "5-Around the world",
+                        duration: "1:52",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/bb9f6653.mp4"
+                    },
+                    {
+                        name: "6-Tricep kickback",
+                        duration: "1:41",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/b9f6653b.mp4"
+                    },
+                    {
+                        name: "7-Frontal raise",
+                        duration: "0:43",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/653b467a.mp4"
+                    },
+                    {
+                        name: "8-Dumbbell overhead high knees",
+                        duration: "0:36",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/9f6653b4.mp4"
+                    },
+                    {
+                        name: "9-Dumbbell side bands",
+                        duration: "0:50",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/f6653b46.mp4"
+                    },
+                    {
+                        name: "10-Russian Twist",
+                        duration: "2:18",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/6653b467.mp4"
+                    },
+                    {
+                        name: "20 min full body",
+                        duration: "18:18",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/English_Video/53b467a0.mp4"
+                    },
+
+                ]
+            },
+            {
+                session: "Session 2",
+                episodes: [
+                    {
+                        name: "1-Backward lunges",
+                        duration: "2:17",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/3b467a0f.mp4"
+                    },
+                    {
+                        name: "2-Suma squat",
+                        duration: "2:11",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/b467a0fb.mp4"
+                    },
+                    {
+                        name: "3-Donkey kickbacks",
+                        duration: "1:48",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/English_Video/467a0fba.mp4"
+                    },
+                    {
+                        name: "4-Lateral raise",
+                        duration: "1:06",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/67a0fbac.mp4"
+                    },
+                    {
+                        name: "5-Bicep Curl",
+                        duration: "1:20",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/7a0fbac8.mp4"
+                    },
+                    {
+                        name: "6-Upward row",
+                        duration: "0:57",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/a0fbac8c.mp4"
+                    },
+                    {
+                        name: "7-Heals reach",
+                        duration: "0:50",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/bac8c42c.mp4"
+                    },
+                    {
+                        name: "8-Crunch",
+                        duration: "0:22",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/English_Video/fbac8c42.mp4"
+                    },
+                    {
+                        name: "9-Side plan",
+                        duration: "1:03",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/English_Video/0fbac8c4.mp4"
+                    },
+
+                    {
+                        name: "Main",
+                        duration: "",
+                        url:""
+                    },
+
+                ]
+            },
+            {
+                session: "Session 3",
+                episodes: [
+                    {
+                        name: "1-Dead lift",
+                        duration: "4:25",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/c8c42c40.mp4"
+                    },
+                    {
+                        name: "2-Step up",
+                        duration: "1:24",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/8c42c405.mp4"
+                    },
+                    {
+                        name: "3-Tricep Extension",
+                        duration: "2:14",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/c42c405f.mp4"
+                    },
+                    {
+                        name: "4-",
+                        duration: "",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/2c405f35.mp4"
+                    },
+                    {
+                        name: "5-Halo",
+                        duration: "0:54",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/2c405f35.mp4"
+                    },
+                    {
+                        name: "Main",
+                        duration: "",
+                        url:""
+                    },
+
+                ]
+            },
+            {
+                session: "Session 4",
+                episodes: [
+                    {
+                        name: "Intro",
+                        duration: "",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/c405f353.mp4"
+                    },
+                    {
+                        name: "1-Side walk",
+                        duration: "1:01",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/405f353a.mp4"
+                    },
+                    {
+                        name: "2-Front kick",
+                        duration: "0:52",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/05f353a7.mp4"
+                    },
+                    {
+                        name: "3-Squat",
+                        duration: "",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/5f353a72.mp4"
+                    },
+                    {
+                        name: "4-Jumping jack",
+                        duration: "0:36",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/f353a72a.mp4"
+                    },
+                    {
+                        name: "5-Chest press",
+                        duration: "1:44",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/353a72a5.mp4"
+                    },
+                    {
+                        name: "6-Pulse",
+                        duration: "0:59",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/53a72a5b.mp4"
+                    },
+                    {
+                        name: "7-Straight lift",
+                        duration: "0:50",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/3a72a5b2.mp4"
+                    },
+                    {
+                        name: "8-Knee raise crunch",
+                        duration: "1:00",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/a72a5b2b.mp4"
+                    },
+                    {
+                        name: "9-Leg lift",
+                        duration: "1:26",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/72a5b2b1.mp4"
+                    },
+                    {
+                        name: "10-Leg spread",
+                        duration: "1:07",
+                        url:"https://nutrostyle.nutrosal.com:4010/videos/workout/2a5b2b19.mp4"
+                    },
+                    {
+                        name: "Main",
+                        duration: "",
+                        url:""
+                    },
+
+
+                ]
+            }
+        ]
     }
 }
 
